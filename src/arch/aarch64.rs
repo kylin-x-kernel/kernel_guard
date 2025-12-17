abe07e53402657f4e354be674de87c8ebbad9f78
luozhixian <luozhixian@kylinos.cn>
2025-12-17 11:03:09 +0800

    support pmr
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>

--- src/arch/aarch64.rs
+++ src/arch/aarch64.rs
@@ -1,5 +1,7 @@
+#[cfg(not(feature = "pmr"))]
 use core::arch::asm;
 
+#[cfg(not(feature = "pmr"))]
 #[inline]
 pub fn local_irq_save_and_disable() -> usize {
     let flags: usize;
@@ -8,7 +10,30 @@ pub fn local_irq_save_and_disable() -> usize {
     flags
 }
 
+#[cfg(not(feature = "pmr"))]
 #[inline]
 pub fn local_irq_restore(flags: usize) {
     unsafe { asm!("msr daif, {}", in(reg) flags) };
+}
+
+/// Only can be used on aarch64 qemu virt now
+#[cfg(feature = "pmr")]
+const GICC_PMR: usize = 0xffff_0000_0800_0004;
+
+#[cfg(feature = "pmr")]
+#[inline]
+pub fn local_irq_save_and_disable() -> usize {
+    let pmr = unsafe {core::ptr::read_volatile((GICC_PMR) as *const u32) as u8};
+    unsafe {
+        core::ptr::write_volatile((GICC_PMR) as *mut u32, 0x80u32);
+    }
+    pmr as usize
+}
+
+#[cfg(feature = "pmr")]
+#[inline]
+pub fn local_irq_restore(flags: usize) {
+    unsafe {
+        core::ptr::write_volatile((GICC_PMR) as *mut u32, flags as u8 as u32);
+    }
 }
